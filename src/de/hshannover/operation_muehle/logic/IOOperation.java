52cd58fe8f17481b613749754eb82c97331dc8a1
Chomper64 <richard.pump@gmx.de>
2012-10-26 13:10:41 +0200

    IOOperations prepped. closes#11

---
+++ src/de/hshannover/operation_muehle/logic/IOOperation.java
@@ -0,0 +1,28 @@
+package de.hshannover.operation_muehle.logic;
+
+public class IOOperation {
+	
+	/**
+	 * Loads the SaveState from the file specified with name.
+	 * @param name Name of the file.
+	 * @return A SaveState from the file.
+	 * @see SaveState
+	 */
+	static SaveState loadGameInfo(String name) {
+		//loads and loads of loading
+	}
+	
+	
+	/**
+	 * Saves the given GameState in a file with the given Name.
+	 * @param name Name of the Savefile.
+	 * @param data The Data thats to be saved.
+	 * @see GameState
+	 */
+	static void saveGameInfo(String name, GameState data) {
+		//Save yourselves!
+	}
+
+
+
+}
