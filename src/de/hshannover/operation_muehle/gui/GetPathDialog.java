abba8148b5c9ff2b7f8fad8e07539bab69db8b70
Julian Haack <Freakside@gmx.net>
2012-10-28 19:49:22 +0100

    GetPathDialog
    das hab ich dazu im internet gefunden vllt können wir da ja irgendeine
    verwendung für finden

--- src/de/hshannover/operation_muehle/gui/GetPathDialog.java
+++ src/de/hshannover/operation_muehle/gui/GetPathDialog.java
@@ -1,5 +1,46 @@
 package de.hshannover.operation_muehle.gui;
 
-public class GetPathDialog {
+import java.beans.PropertyChangeEvent; 
+import java.beans.PropertyChangeListener; 
+import java.io.File; 
 
-}
+import javax.swing.JFileChooser; 
+
+public class GetPathDialog { 
+
+    public static void main(String[] args) { 
+    	GetPathDialog odc = new GetPathDialog(); 
+        odc.open(); 
+    } 
+
+    private void open() { 
+        final JFileChooser chooser = new JFileChooser("Verzeichnis wählen"); 
+        chooser.setDialogType(JFileChooser.OPEN_DIALOG); 
+        chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        final File file = new File("/home"); 
+
+        chooser.setCurrentDirectory(file); 
+
+        chooser.addPropertyChangeListener(new PropertyChangeListener() { 
+            public void propertyChange(PropertyChangeEvent e) { 
+                if (e.getPropertyName().equals(JFileChooser.SELECTED_FILE_CHANGED_PROPERTY) 
+                        || e.getPropertyName().equals(JFileChooser.DIRECTORY_CHANGED_PROPERTY)) { 
+                    final File f = (File) e.getNewValue(); 
+                } 
+            } 
+        }); 
+
+        chooser.setVisible(true); 
+        final int result = chooser.showOpenDialog(null); 
+        
+        /**
+         * Ausgabe
+         */
+        
+        if (result == JFileChooser.APPROVE_OPTION) { 
+            File inputVerzFile = chooser.getSelectedFile(); 
+            String inputVerzStr = inputVerzFile.getPath(); 
+        } 
+        chooser.setVisible(false); 
+    } 
+} 
