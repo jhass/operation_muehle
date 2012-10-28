955fe7cd92d91d8f45e97310573aece6be26f9b8
Julian Haack <Freakside@gmx.net>
2012-10-28 15:46:55 +0100

    test first commt :P updated LogWindow
    method setLog incomplete

--- src/de/hshannover/operation_muehle/gui/LogWindow.java
+++ src/de/hshannover/operation_muehle/gui/LogWindow.java
@@ -1,5 +1,74 @@
 package de.hshannover.operation_muehle.gui;
 
-public class LogWindow {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JList;
 
+/**LogWindow shows the game log
+ * 
+ * @author Julian Haack
+ */
+
+public class LogWindow extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * LogWindow Frame
+	 */
+	
+	private LogWindow() {
+		setTitle("Log");
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		setBounds(100, 100, 200, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JList jlistLog = new JList();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(jlistLog, GroupLayout.DEFAULT_SIZE, 154, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(jlistLog, GroupLayout.DEFAULT_SIZE, 230, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		contentPane.setLayout(gl_contentPane);
+	}
+	
+	/**
+	 * Hide LogWindow Frame
+	 */
+	
+	public void Hide() {
+		setVisible(false);
+	}
+	
+	/**
+	 * Show LogWindow Frame
+	 */
+	
+	public void Show() {
+		setVisible(true);
+	}
+	
+	/**
+	 * Add Item to JList
+	 * ToDo:
+	 */
+	
+	public void SetLog() {
+		
+	}
 }
