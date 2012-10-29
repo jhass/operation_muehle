bd98c97509d4326a06cd1f3cdc4caac9a62df5f5
zyklos <sirotin8@web.de>
2012-10-29 05:45:28 +0100

    mod: moved StrategyLoader to ClassFileLoader mod: ClassLoader is marked as deprecated

--- src/de/hshannover/operation_muehle/utils/loader/ClassLoader.java
+++ src/de/hshannover/operation_muehle/utils/loader/ClassLoader.java
@@ -7,58 +7,60 @@ import java.util.Enumeration;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
-
-/** Generic class to fetch classes from JARs in a directory
+/**
+ * Generic class to fetch classes from JARs in a directory
  * 
- *
+ * @deprecated ClassLoader exists in java.lang package. use StrategyLoader only.
+ * 
  */
 public class ClassLoader {
 	private ArrayList<String> availableClasses = new ArrayList<String>();
-	
-	
+
 	/**
 	 * 
-	 * @param searchPath directory to seach for JARs
-	 * @throws IOException If there's an error reading one of the JARs
-	 * @throws IllegalArgumentException if searchPath is not a directory
+	 * @param searchPath
+	 *            directory to seach for JARs
+	 * @throws IOException
+	 *             If there's an error reading one of the JARs
+	 * @throws IllegalArgumentException
+	 *             if searchPath is not a directory
 	 */
-	public ClassLoader(String searchPath)
-		throws IOException {
+	public ClassLoader(String searchPath) throws IOException {
 		File directory = new File(searchPath);
 		if (!directory.isDirectory()) {
-			throw new IllegalArgumentException(searchPath+" is no directory");
+			throw new IllegalArgumentException(searchPath + " is no directory");
 		}
-		
+
 		File[] files = new File(searchPath).listFiles();
 		for (File file : files) {
 			if (file.getName().endsWith(".jar")) {
 				JarFile jar = new JarFile(file);
-				JarEntry entry; 
-			
-				for (Enumeration<JarEntry> entries = jar.entries();
-						entries.hasMoreElements();) {
+				JarEntry entry;
+
+				for (Enumeration<JarEntry> entries = jar.entries(); entries
+						.hasMoreElements();) {
 					entry = entries.nextElement();
 					if (entry.getName().endsWith(".class")) {
 						entry = entries.nextElement();
 						availableClasses.add(entry.getName()
-								.replaceAll("/", ".")
-								.replaceAll(".class", ""));
+								.replaceAll("/", ".").replaceAll(".class", ""));
 					}
 				}
-			
+
 				jar.close();
 			}
 		}
 	}
-	
-	/** Get a list of instances for all classes that can
-	 *  be instantiated with a default constructor.
+
+	/**
+	 * Get a list of instances for all classes that can be instantiated with a
+	 * default constructor.
 	 * 
 	 * @return
 	 */
 	public ArrayList<Object> getAllInstances() {
 		ArrayList<Object> instances = new ArrayList<Object>();
-		
+
 		for (String klass : availableClasses) {
 			try {
 				instances.add(getInstance(klass));
@@ -67,41 +69,42 @@ public class ClassLoader {
 			} catch (IllegalAccessException e) {
 			}
 		}
-		
-		
+
 		return instances;
 	}
-	
-	
-	/** Gets an instance of the given class using the default
-	 *  constructor.
+
+	/**
+	 * Gets an instance of the given class using the default constructor.
 	 * 
-	 * @param klass the name of the class
+	 * @param klass
+	 *            the name of the class
 	 * @return The instance
-	 * @throws ClassNotFoundException if the given class isn't available
-	 * @throws InstantiationException if the given class can't be instantiated
-	 * @throws IllegalAccessException if the given class isn't public
+	 * @throws ClassNotFoundException
+	 *             if the given class isn't available
+	 * @throws InstantiationException
+	 *             if the given class can't be instantiated
+	 * @throws IllegalAccessException
+	 *             if the given class isn't public
 	 */
-	public Object getInstance(String klass)
-			throws ClassNotFoundException,
-			       InstantiationException,
-			       IllegalAccessException {
+	public Object getInstance(String klass) throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException {
 		return getClass(klass).newInstance();
 	}
-	
-	/** Returns the Class of the given class
+
+	/**
+	 * Returns the Class of the given class
 	 * 
 	 * @param klass
 	 * @return the Class object for klass
-	 * @throws ClassNotFoundException if the class isn't available
+	 * @throws ClassNotFoundException
+	 *             if the class isn't available
 	 */
-	private Class<?> getClass(String klass)
-		throws ClassNotFoundException {
+	private Class<?> getClass(String klass) throws ClassNotFoundException {
 		if (availableClasses.contains(klass)) {
 			return Class.forName(klass);
 		} else {
-			throw new ClassNotFoundException("Can't find "+klass);
+			throw new ClassNotFoundException("Can't find " + klass);
 		}
 	}
-	
+
 }
