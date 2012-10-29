eb03953b2e35676d5f76d54259886c25ab1903bf
zyklos <sirotin8@web.de>
2012-10-29 17:08:55 +0100

    mod: added some print messages in Test Classes

--- src/examples/reflection/TestAlex.java
+++ src/examples/reflection/TestAlex.java
@@ -1,63 +1,31 @@
 package examples.reflection;
 
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.Iterator;
-import java.util.jar.JarEntry;
 
-import de.hshannover.inform.muehle.strategy.Move;
-import de.hshannover.inform.muehle.strategy.Slot;
-import de.hshannover.operation_muehle.utils.loader.ClassFileLoader;
+import de.hshannover.operation_muehle.utils.loader.StrategyLoader;
 import de.hshannover.inform.muehle.strategy.Strategy;
 
-public class TestAlex implements Strategy {
+public class TestAlex{
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		ClassFileLoader cfl = new ClassFileLoader(
+		StrategyLoader cfl = new StrategyLoader(
 				"/home/zero/workspace/operation_muehle/src/examples/reflection");
-		ArrayList a = cfl.getClassesOfType(String.class);
-		for (Iterator i = a.iterator(); i.hasNext();) {
-			Object o = i.next();
-			System.out.println(o.toString());
-			Strategy test = (Strategy)((Class)o).newInstance();
-			test.doMove(null, null, 0);
-			test.getStrategyName();
-			test.placeStone(null, null, 0);
+		ArrayList<Class<?>> a = cfl.getClasses(String.class);
+		for (Iterator<Class<?>> i = a.iterator(); i.hasNext();) {
+			Class<?> o = i.next();
+			Strategy s = (Strategy)o.newInstance();
+			System.out.println("=========================================");
+			System.out.println("Class: "+o);
+			System.out.println("Strategy: "+s.getStrategyName());
+			System.out.println("Group: "+s.getGroup());
+			System.out.println("doMove: "+s.doMove(null, null, 0));
+			System.out.println("placeStone: "+s.placeStone(null, null, 0));
+			System.out.println("removeStone: "+s.removeStone(0));
 		}
-	}
-
-	@Override
-	public Move doMove(Move arg0, Slot arg1, int arg2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getGroup() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public String getStrategyName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Slot placeStone(Slot arg0, Slot arg1, int arg2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Slot removeStone(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
--- src/examples/reflection/TestKI.java
+++ src/examples/reflection/TestKI.java
@@ -1,43 +1,28 @@
-
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.jar.JarEntry;
+package examples.reflection;
 
 import de.hshannover.inform.muehle.strategy.Move;
 import de.hshannover.inform.muehle.strategy.Slot;
-import de.hshannover.operation_muehle.utils.loader.ClassFileLoader;
 
 public class TestKI implements de.hshannover.inform.muehle.strategy.Strategy {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		ClassFileLoader cfl = new ClassFileLoader(".");
-		ArrayList a = cfl.getClassesOfType(String.class);
-		for (Iterator i = a.iterator(); i.hasNext();) {
-			Object o = i.next();
-			System.out.println(o.toString());
-		}
-	}
 
 	@Override
 	public Move doMove(Move arg0, Slot arg1, int arg2) {
 		System.out.println("do Move!!");
 		// TODO Auto-generated method stub
-		return null;
+		return new de.hshannover.operation_muehle.logic.Move(
+				new de.hshannover.operation_muehle.logic.Slot(7,7,0));
 	}
 
 	@Override
 	public int getGroup() {
+		System.out.println("get Group!");
 		// TODO Auto-generated method stub
-		return 0;
+		return 999999;
 	}
 
 	@Override
 	public String getStrategyName() {
+		System.out.println("test KKKIII!! BANSAI!!!!!!!");
 		// TODO Auto-generated method stub
 		return "testKI";
 	}
@@ -46,13 +31,14 @@ public class TestKI implements de.hshannover.inform.muehle.strategy.Strategy {
 	public Slot placeStone(Slot arg0, Slot arg1, int arg2) {
 		// TODO Auto-generated method stub
 		System.out.println("place Stone!!");
-		return null;
+		return new de.hshannover.operation_muehle.logic.Slot(7,7,0);
 	}
 
 	@Override
 	public Slot removeStone(int arg0) {
 		// TODO Auto-generated method stub
-		return null;
+		System.out.println("remove Stone!!!");
+		return new de.hshannover.operation_muehle.logic.Slot(7,7,0);
 	}
 
 }
