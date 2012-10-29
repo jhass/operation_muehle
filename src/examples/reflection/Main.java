e099f92d08b6bcf77ca201b9eea2a9895a92d40d
zyklos <sirotin8@web.de>
2012-10-29 18:15:11 +0100

    del: removed catch direction in Test Main

--- src/examples/reflection/Main.java
+++ src/examples/reflection/Main.java
@@ -7,14 +7,10 @@ import de.hshannover.operation_muehle.utils.loader.StrategyLoader;
 
 public class Main {
 	public static void main(String[] args) {
-		try {
-			StrategyLoader loader = new StrategyLoader("./ki/");
-			System.out.println("Available strategies:");
-			for (Strategy strategy : loader.getAllStrategies()) {
-				System.out.println(strategy.getStrategyName());
-			}
-		} catch (IOException e) {
-			System.out.println("malicious content in ki folder");
+		StrategyLoader loader = new StrategyLoader("./ki/");
+		System.out.println("Available strategies:");
+		for (Strategy strategy : loader.getAllStrategies()) {
+			System.out.println(strategy.getStrategyName());
 		}
 	}
 }
