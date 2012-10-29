b36b3319e11d59e312c9f370b4d4bf3e506b019d
zyklos <sirotin8@web.de>
2012-10-29 18:31:08 +0100

    add: simple Runner added in package operation_muehle.logic

---
+++ src/de/hshannover/operation_muehle/logic/Runner.java
@@ -0,0 +1,20 @@
+package de.hshannover.operation_muehle.logic;
+
+import de.hshannover.operation_muehle.Facade;
+import de.hshannover.operation_muehle.gui.GUIController;
+
+public class Runner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Facade facade = Facade.getInstance();
+		
+		GUIController gui = new GUIController();
+		
+		
+
+	}
+
+}
