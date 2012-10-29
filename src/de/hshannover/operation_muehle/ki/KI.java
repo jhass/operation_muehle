cdaf91edd5a52b9db6d87d002dc1d0d28be05174
zyklos <sirotin8@web.de>
2012-10-29 17:11:29 +0100

    mod: KI is almost done. Only the algorithms are missing.

--- src/de/hshannover/operation_muehle/ki/KI.java
+++ src/de/hshannover/operation_muehle/ki/KI.java
@@ -9,7 +9,7 @@ import de.hshannover.inform.muehle.strategy.Strategy;
 /**
  * 
  * @author zyklos
- *
+ * 
  */
 public class KI implements Strategy {
 
@@ -20,27 +20,41 @@ public class KI implements Strategy {
 	 * NIKI for 'Nicht Intelligente KI'
 	 */
 	private String strategyName = "NIKI";
+	/**
+	 * we are the second group
+	 */
+	private int group = 2;
 	private Gameboard boardIPlayWith;
 
-	private Move lastKnownMove;
-	private Slot lastKnownRemoved;
-
 	/**
-	 * Required for creating Slots and Moves
+	 * Required for creating Slots and Moves First we think we are black
 	 */
-	private int color;
+	private int color = BLACK;
 
 	/**
-	 * Initialize the own Gameboard und set color to WHITE
+	 * Time-Management
+	 */
+	private long time;
+	private long timeStamp;
+
+	/**
+	 * Initialize the own Gameboard
 	 */
 	public KI() {
 		this.boardIPlayWith = new Gameboard();
-		this.color = WHITE;
 	}
 
-	public KI(int color) {
-		this();
-		this.color = color;
+	private void initStamp(long time) {
+		this.timeStamp = System.currentTimeMillis();
+		this.time = time;
+	}
+
+	private long stamp() {
+		long current = System.currentTimeMillis();
+		long diff = current - timeStamp;
+		this.timeStamp = current;
+		this.time -= diff;
+		return this.time;
 	}
 
 	private int getOpponentColor() {
@@ -52,22 +66,42 @@ public class KI implements Strategy {
 	}
 
 	/**
-	 * The Slot-Interface uses chars to specify columns.
-	 * The address-space for the columns is between 'A' and 'G' 
-	 * like in common Muehle game.
-	 * @params col Muehle column-address, that won't be checked,
-	 * 				if it lies in the valid address-space
-	 * @return 		returns the equivalent int value 
-	 * 				
+	 * The Slot-Interface uses chars to specify columns. The address-space for
+	 * columns is between 'A' and 'G' like in common Muehle game.
+	 * 
+	 * @params col Muehle column-address, that won't be checked, if it lies in
+	 *         the valid address-space
+	 * @return returns the equivalent int value
+	 * 
 	 */
 	private int charToIntCoord(char col) {
 		return (int) col - 64;
 	}
 
+	private void apply(de.hshannover.inform.muehle.strategy.Move last) {
+		if (last != null) {
+			this.boardIPlayWith.applyMove(new Move(
+
+			new Slot(last.fromSlot().getRow(), this.charToIntCoord(last
+					.fromSlot().getColumn()), this.getOpponentColor()),
+
+			new Slot(last.toSlot().getRow(), this.charToIntCoord(last.toSlot()
+					.getColumn()), this.getOpponentColor())
+
+			));
+		}
+	}
+
+	private void remove(de.hshannover.inform.muehle.strategy.Slot removed) {
+		if (removed != null) {
+			this.boardIPlayWith.removeStone(new Slot(removed.getRow(), this
+					.charToIntCoord(removed.getColumn()), this.color));
+		}
+	}
+
 	@Override
 	public int getGroup() {
-		// We are the second group
-		return 2;
+		return this.group;
 	}
 
 	@Override
@@ -76,103 +110,72 @@ public class KI implements Strategy {
 	}
 
 	@Override
-	public de.hshannover.inform.muehle.strategy.Move doMove(
-			de.hshannover.inform.muehle.strategy.Move last,
+	public Move doMove(de.hshannover.inform.muehle.strategy.Move last,
 			de.hshannover.inform.muehle.strategy.Slot removed, int thinktime) {
-		
-		de.hshannover.inform.muehle.strategy.Move result; 
-		
-		long time = thinktime;
-		long lastTimeStamp = System.currentTimeMillis();
-		long timeDiff;
-		
-		if (last != null) {
-			this.boardIPlayWith.applyMove(
-					new Move(
-							
-							new Slot(last.fromSlot().getRow(),
-		 this.charToIntCoord(last.fromSlot().getColumn()), 
-		 this.getOpponentColor()), 
-		 
-		 					new Slot(last.toSlot().getRow(), 
-		 this.charToIntCoord(last.toSlot().getColumn()),
-		 this.getOpponentColor())
-							
-					)
-			);
-		}
-		if (removed != null) {
-			this.boardIPlayWith.removeStone(
-		 		new Slot(removed.getRow(), 
-		 				this.charToIntCoord(removed.getColumn()),
-		 				this.getOpponentColor())
-			);
-		}
-		timeDiff = System.currentTimeMillis() - lastTimeStamp;
-		time -= timeDiff;
-		
+
+		Move result;
+
+		this.initStamp(thinktime);
+
+		apply(last);
+		remove(removed);
+
+		this.stamp();
+
 		/*
-		 * TODO Here the algorithm for choosing and creating a
-		 * move
+		 * TODO Here the algorithm for choosing and creating a move
 		 */
-		result = null;
+		result = new Move(new Slot(6,6,this.color));
 
 		return result;
 	}
 
 	@Override
-	public de.hshannover.inform.muehle.strategy.Slot placeStone(
-			de.hshannover.inform.muehle.strategy.Slot last,
+	public Slot placeStone(de.hshannover.inform.muehle.strategy.Slot last,
 			de.hshannover.inform.muehle.strategy.Slot removed, int thinktime) {
-		
-		de.hshannover.inform.muehle.strategy.Slot result;
+
+		Slot result;
 
 		/*
-		 *  TODO Man braeuchte noch ein applySlot bei Gameboard,
-		 *  jedenfalls kann man dafuer das derzeitige applyMove 
-		 *  nicht benutzen  
+		 * TODO Man braeuchte noch ein applySlot bei Gameboard, jedenfalls kann
+		 * man dafuer das derzeitige applyMove nicht benutzen
 		 */
 		if (last != null) {
-			// geht, da NullPointerException in applyMove
-			this.boardIPlayWith.applyMove(
-					new Move(	
-							null,
-		 
-		 					new Slot(last.getRow(), 
-		 				this.charToIntCoord(last.getColumn()),
-		 				this.getOpponentColor())
-							
-					)
-			);
+
+			// geht nicht, da NullPointerException in applyMove
+			this.boardIPlayWith.applyMove(new Move(null,
+
+			new Slot(last.getRow(), this.charToIntCoord(last.getColumn()), this
+					.getOpponentColor())
+
+			));
+		} else {
+			// this case should only come once:
+			// this is the first move or place in that game and we are in action
+			// this means we are white not black
+			this.color = WHITE;
 		}
 
-		if (removed != null) {
-			this.boardIPlayWith.removeStone(
-			 		new Slot(removed.getRow(), 
-			 				this.charToIntCoord(removed.getColumn()),
-			 				this.getOpponentColor())
-				);
-		}
+		remove(removed);
 
 		/*
-		 * TODO ... choosing the Stone, that has to be removed ... 
+		 * TODO ... choosing the Stone, that has to be removed ...
 		 */
-		result = null;
-		
+		result = new Slot(5,5,this.color);
+
 		return result;
 	}
 
 	@Override
-	public de.hshannover.inform.muehle.strategy.Slot removeStone(
-			int thinktime) {
-		
-		de.hshannover.inform.muehle.strategy.Slot result;
+	public Slot removeStone(int thinktime) {
+
+		Slot result;
 		/*
-		 * TODO Hier wird der Algorithmus fuer Auswaehlen
-		 * berechnet
+		 * TODO Hier wird der Algorithmus fuer Auswaehlen berechnet
 		 */
-		result = null;
-		
+
+		result = new Slot(4, 4, this.getOpponentColor());
+
 		return result;
 	}
 
