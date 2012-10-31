1cf346bb9e5bb76344d5a6de40c962f34e783c1b
Chomper64 <richard.pump@gmx.de>
2012-10-31 11:23:59 +0100

    Filled IOOperations. Need feedback.

--- src/de/hshannover/operation_muehle/logic/IOOperation.java
+++ src/de/hshannover/operation_muehle/logic/IOOperation.java
@@ -1,5 +1,12 @@
 package de.hshannover.operation_muehle.logic;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class IOOperation {
 	
 	/**
@@ -9,7 +16,27 @@ public class IOOperation {
 	 * @see SaveState
 	 */
 	public static SaveState loadGameInfo(String name) {
-		//loads and loads of loading
+		SaveState s = null; //Eclipse complaining if not initializing here....
+		Boolean problems = true;
+		while(problems) {
+			try {
+				FileInputStream file = new FileInputStream(name);
+				ObjectInputStream pump = new ObjectInputStream(file);
+				s = (SaveState) pump.readObject();
+				pump.close();
+				problems = false;
+			} catch (FileNotFoundException e) {
+				System.out.println("Can't find/acces file "+name);
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Errormessage?
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Someone help me with this one, whats the problem?
+				e.printStackTrace();
+			}
+		}
+		return s;
 	}
 	
 	
@@ -20,7 +47,23 @@ public class IOOperation {
 	 * @see GameState
 	 */
 	public static void saveGameInfo(String name, GameState data) {
-		//Save yourselves!
+		boolean problems = true;
+		while(problems) {
+			try {
+				FileOutputStream file = new FileOutputStream(name);
+				ObjectOutputStream pump = new ObjectOutputStream(file);
+				pump.writeObject(data);
+				pump.close();
+				problems = false;
+				System.out.println("Game Saved"); //GUI should print this. How?
+			} catch (FileNotFoundException e) {
+				System.out.println("Can't create/access the file "+name); //TODO Errormessage?
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Errormessage?
+				e.printStackTrace();
+			}
+		}
 	}
 
 
