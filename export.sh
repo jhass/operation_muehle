c13c756e41140684fb922ca88e3e6af363bc06a0
Jonne Haß <me@mrzyx.de>
2012-12-22 10:22:01 +0100

    condense calls to ant in export.sh to a single one

--- export.sh
+++ export.sh
@@ -7,13 +7,11 @@ export_ais="Benni Richard  Alex"
 rm $export_dir/**/*.jar
 rm "$zipname.zip"
 
-ant create_run_jar
-ant create_alex_ai_jar
-ant create_richard_ai_jar
-ant create_benni_ai_jar
-ant create_test_ais_jar
-
-#cp lib/*AI*.jar "$export_dir/lib/"
+ant create_run_jar \
+    create_alex_ai_jar \
+    create_richard_ai_jar \
+    create_benni_ai_jar \
+    create_test_ais_jar
 
 for ai in $export_ais; do
   cp "lib/${ai}AI.jar" "$export_dir/lib/"
